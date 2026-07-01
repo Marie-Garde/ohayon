32653973cc52fc785220e3f2a69edee16c4ca472
Jessalynn CHOBY <j.choby@proton.me>
2026-07-01 16:02:05 +0200

    remove stats bar from qui-sommes-nous page

--- app/pages/qui-sommes-nous.vue
+++ app/pages/qui-sommes-nous.vue
@@ -5,7 +5,6 @@
     <QuiSommesNousTeamSection />
     <QuiSommesNousPillarsSection />
     <QuiSommesNousValuesSection />
-    <QuiSommesNousStatsBar />
-    <HomeCtaSection />
+<HomeCtaSection />
   </div>
 </template>
